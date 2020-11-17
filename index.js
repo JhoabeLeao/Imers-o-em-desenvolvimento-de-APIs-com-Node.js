6fd7e47a903284a05d746a7f1b6d7c624efe36d4
JhoabeLeao <jhoabe.leao@hotmail.com>
2020-11-16 21:14:38 -0300

    Promises test

--- index.js
+++ index.js
@@ -4,23 +4,30 @@
 2 - Obter o endereço do usuario pelo Id
 */
 
-function obterUsuario(callback) {
-    setTimeout( function(){
-        return callback(null, {
-            id: 1,
-            nome: 'Aladin',
-            dataNascimento: new Date()
-        })
-    }, 1000)
+function obterUsuario() {
+    //quando der algum proble -> reject(erro)
+    //quando der sucesso -> RESOLVE
+    return new Promise(function resolvePromise(resolve, reject){
+        setTimeout( function(){
+            return resolve( {
+                id: 1,
+                nome: 'Aladin',
+                dataNascimento: new Date()
+            })
+        }, 1000)
+    })
+    
 }
 
-function obterTelefone(idUsuario, callback) {
-    setTimeout(() => {
-        return callback(null, {
-            telefone: '1234567',
-            ddd: 11
-        })
-    }, 2000);
+function obterTelefone(idUsuario, ) {
+    return new Promise(function resolvePromise(resolve, reject){
+        setTimeout(() => {
+            return resolve ({
+                telefone: '1234567',
+                ddd: 11
+            })
+        }, 2000);
+    })
 
 }
 
@@ -33,11 +40,30 @@ function obterEndereco(idUsuario, callback) {
     }, 2000);
 }
 
-function resolverUsuario(erro, usuario) {
-    console.log('usuario', usuario)
-}
+const usuarioPromise = obterUsuario()
+//para manipular com sucesso usamos a função .then
+//para manipular erro .catch
+usuarioPromise
+    .then(function (usuario){
+        return obterTelefone(usuario.id)
+        .then(function resolverTelefone(result){
+            return {
+                usuario: {
+                    nome: usuario.nome,
+                    id: usuario.id
+                },
+                telefone: result
+            }
+        })
+    })
+    .then(function (resultado) {
+        console.log('resultado', resultado)
+    })
+    .catch(function (error) {
+        console.error('DEU RUIM', error)
+    })
 
-obterUsuario(function resolverUsuario(error, usuario){
+/*obterUsuario(function resolverUsuario(error, usuario){
     if(error) {
         console.error('DEU RUIM em USUARIO', error)
         return;
@@ -64,4 +90,4 @@ obterUsuario(function resolverUsuario(error, usuario){
 })
 //const telefone = obterTelefone(usuario.id)
 
-//console.log('telefone', telefone)
+//console.log('telefone', telefone)*/
